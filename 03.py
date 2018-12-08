30a6a49b1938d4e9857faecd26951b6c1806e5a5
ELizabeth-III <iseultsky@gmail.com>
2018-12-07 23:24:03 -0200

    Adicionando outros exercícios

---
+++ 03.py
@@ -0,0 +1,3 @@
+n = int(input('Digite um número'))
+n1 = int(input('Digite outro número'))
+print('A soma dos valores é de',(n+n1))
---
+++ 05.py
@@ -0,0 +1,3 @@
+medida = float(input('Digite um número em metros: '))
+cm =  medida * 100
+print('A medida de metros {} em cm é de {}'.format(medida, cm))
